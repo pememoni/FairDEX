0e5c2d765065bc30d0cd80e7caa5189d2bb91a12
peyman <pmomeni@uwaterloo.ca>
2022-05-18 22:49:48 -0400

    Added Swap and Liquidity functions

--- packages/hardhat/deploy/00_deploy_balloons_dex.js
+++ packages/hardhat/deploy/00_deploy_balloons_dex.js
@@ -18,16 +18,24 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     log: true,
   });
 
-  // const dex = await ethers.getContract("DEX", deployer);
+  const dex = await ethers.getContract("DEX", deployer);
 
   // paste in your address here to get 10 balloons on deploy:
-  // await balloons.transfer("YOUR_ADDRESS",""+(10*10**18));
+  await balloons.transfer(
+    "0xe5Ac4b30d4598568c257f26662Bf1384FE2cad0e",
+    "" + 10 * 10 ** 18
+  );
 
   // uncomment to init DEX on deploy:
-  // console.log("Approving DEX ("+dex.address+") to take Balloons from main account...")
+  console.log(
+    "Approving DEX (" + dex.address + ") to take Balloons from main account..."
+  );
   // If you are going to the testnet make sure your deployer account has enough ETH
-  // await balloons.approve(dex.address,ethers.utils.parseEther('100'));
-  // console.log("INIT exchange...")
-  // await dex.init(""+(3*10**18),{value:ethers.utils.parseEther('3'),gasLimit:200000})
+  await balloons.approve(dex.address, ethers.utils.parseEther("100"));
+  console.log("INIT exchange...");
+  await dex.init("" + 3 * 10 ** 18, {
+    value: ethers.utils.parseEther("3"),
+    gasLimit: 200000,
+  });
 };
 module.exports.tags = ["Balloons", "DEX"];
